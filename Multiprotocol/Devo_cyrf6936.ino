3b3b61f52ce3926fe89f340225340d7b65a360a7
pascallanger <planger@mathworks.com>
2016-08-25 15:44:02 +0200

    DEVO channel order

--- Multiprotocol/Devo_cyrf6936.ino
+++ Multiprotocol/Devo_cyrf6936.ino
@@ -143,7 +143,7 @@ static void __attribute__((unused)) DEVO_build_data_pkt()
 	uint8_t sign = 0x0b;
 	for (uint8_t i = 0; i < 4; i++)
 	{
-		int16_t value=map(Servo_data[ch_idx * 4 + i],servo_min_125,servo_max_125,-1600,1600);//range -1600..+1600
+		int16_t value=map(Servo_data[CH_EATR[ch_idx * 4 + i]],servo_min_125,servo_max_125,-1600,1600);//range -1600..+1600
 		if(value < 0)
 		{
 			value = -value;

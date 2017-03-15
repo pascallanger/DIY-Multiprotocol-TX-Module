8b8b26e67597249e4ce14667e7b9c3880534d680
Dennis <soligen2010@gmail.com>
2017-03-14 20:50:50 -0400

    Opened up a free bit in the option byte for future use

--- Multiprotocol/CABELL_nrf224l01.ino
+++ Multiprotocol/CABELL_nrf224l01.ino
@@ -42,7 +42,7 @@ Multiprotocol is distributed in the hope that it will be useful,
 #define CABELL_BIND_RADIO_ADDR  0xA4B7C123F7LL
 
 #define CABELL_OPTION_MASK_CHANNEL_REDUCTION     0x0F
-#define CABELL_OPTION_MASK_RECIEVER_OUTPUT_MODE  0x70
+#define CABELL_OPTION_MASK_RECIEVER_OUTPUT_MODE  0x30
 #define CABELL_OPTION_SHIFT_RECIEVER_OUTPUT_MODE 4
 #define CABELL_OPTION_MASK_MAX_POWER_OVERRIDE    0x80
 
@@ -56,10 +56,11 @@ typedef struct {
    uint8_t  reserved = 0;
    uint8_t  option;
                           /*   mask 0x0F    : Channel reduction.  The number of channels to not send (subtracted frim the 16 max channels) at least 4 are always sent
-                           *   mask 0x70>>4 : Reciever outout mode
-                           *                  1 = Single PPM on individual pins for each channel 
-                           *                  2 = SUM PPM on channel 1 pin
-                           *   mask 0x80>>7   Unused by RX.  Contains max power override flag
+                           *   mask 0x30>>4 : Reciever outout mode
+                           *                  0 = Single PPM on individual pins for each channel 
+                           *                  1 = SUM PPM on channel 1 pin
+                           *   mask 0x40>>7   Unused 
+                           *   mask 0x80>>7   Unused by RX.  Contains max power override flag for Multiprotocol T module
                            */  
    uint8_t  modelNum;
    uint16_t checkSum; 

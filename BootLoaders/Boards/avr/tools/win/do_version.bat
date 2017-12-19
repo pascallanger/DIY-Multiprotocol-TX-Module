2283ef0523bad5fb11f62be66b737409260f7fba
Ben Lye <ben@lye.co.nz>
2017-12-19 11:49:54 +0000

    Tweak windows version scripts

--- BootLoaders/Boards/avr/tools/win/do_version.bat
+++ BootLoaders/Boards/avr/tools/win/do_version.bat
@@ -1,22 +1,18 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
 
-REM SET DEBUG=1
+SET DEBUG=1
 
 SET BUILD_PATH=%1
 SET PROJECT_NAME=%2
 SET SKETCH_PATH=%3
 SET MULTI_BOARD=%4
-SET EXPORT_FLAG=%5
+SET BOARD_VERSION=%5
+SET EXPORT_FLAG=%6
 
 REM Remove double-quotes from the paths
 SET BUILD_PATH=%BUILD_PATH:"=%
 SET SKETCH_PATH=%SKETCH_PATH:"=%
-
-IF DEFINED DEBUG (
-	ECHO Sketch Path: %SKETCH_PATH%
-	ECHO Multi board: %MULTI_BOARD%
-)
 
 IF %MULTI_BOARD%==MULTI_NO_BOOT SET MULTI_TYPE=avr
 IF %MULTI_BOARD%==MULTI_FLASH_FROM_TX SET MULTI_TYPE=avr
@@ -24,8 +20,16 @@ IF %MULTI_BOARD%==MULTI_STM32_NO_BOOT SET MULTI_TYPE=stm
 IF %MULTI_BOARD%==MULTI_STM32_FLASH_FROM_TX SET MULTI_TYPE=stm
 IF %MULTI_BOARD%==MULTI_ORANGERX SET MULTI_TYPE=orx
 
+IF DEFINED DEBUG (
+  ECHO.
+	ECHO Sketch Path: %SKETCH_PATH%
+	ECHO Multi Board: %MULTI_BOARD%
+  ECHO Multi Board Type: %MULTI_TYPE%
+  ECHO.
+)
+
 IF EXIST "%BUILD_PATH%\sketch\Multiprotocol.h" (
-  IF DEFINED DEBUG ECHO Getting Multi-MODULE firmware version from "%BUILD_PATH%\sketch\Multiprotocol.h"
+  IF DEFINED DEBUG ECHO Getting Multi firmware version from "%BUILD_PATH%\sketch\Multiprotocol.h"
   FOR /F "tokens=* usebackq" %%A in (`%SystemRoot%\system32\findstr.exe /C:"#define VERSION_MAJOR" "%BUILD_PATH%\sketch\Multiprotocol.h"`) DO FOR /F "tokens=3" %%i in ("%%A") do SET MAJOR_VERSION=%%i
   FOR /F "tokens=* usebackq" %%B in (`%SystemRoot%\system32\findstr.exe /C:"#define VERSION_MINOR" "%BUILD_PATH%\sketch\Multiprotocol.h"`) DO FOR /F "tokens=3" %%i in ("%%B") do SET MINOR_VERSION=%%i
   FOR /F "tokens=* usebackq" %%C in (`%SystemRoot%\system32\findstr.exe /C:"#define VERSION_REVISION" "%BUILD_PATH%\sketch\Multiprotocol.h"`) DO FOR /F "tokens=3" %%i in ("%%C") do SET REVISION_VERSION=%%i
@@ -35,7 +39,11 @@ IF EXIST "%BUILD_PATH%\sketch\Multiprotocol.h" (
   SET MULTI_VER=
 )
 
-IF DEFINED DEBUG ECHO Multi-MODULE firmware version: %MULTI_VER%
+IF DEFINED DEBUG (
+  ECHO.
+  ECHO Multi Firmware Version: %MULTI_VER%
+  ECHO.
+)
 
 REM Copy the compiled file to the sketch folder with the version number in the file name
 IF EXIST "%BUILD_PATH%\%PROJECT_NAME%.hex" (

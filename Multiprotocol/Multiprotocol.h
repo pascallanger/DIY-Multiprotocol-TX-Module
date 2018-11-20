d990ebe49ef88565c8a9856257c1796c89e33e6f
Pascal Langer <planger@mathworks.com>
2018-11-20 21:15:31 +0100

    _Config.h file update

--- Multiprotocol/Multiprotocol.h
+++ Multiprotocol/Multiprotocol.h
@@ -19,7 +19,7 @@
 #define VERSION_MAJOR		1
 #define VERSION_MINOR		2
 #define VERSION_REVISION	1
-#define VERSION_PATCH_LEVEL	18
+#define VERSION_PATCH_LEVEL	19
 
 //******************
 // Protocols

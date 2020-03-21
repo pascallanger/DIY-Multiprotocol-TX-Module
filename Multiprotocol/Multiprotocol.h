3df836e6b857ffb402b7b93c9354385fb24aaadd
Pascal Langer <planger@mathworks.com>
2020-03-21 19:00:40 +0100

    Hitec: fix a bind issue

--- Multiprotocol/Multiprotocol.h
+++ Multiprotocol/Multiprotocol.h
@@ -19,7 +19,7 @@
 #define VERSION_MAJOR		1
 #define VERSION_MINOR		3
 #define VERSION_REVISION	0
-#define VERSION_PATCH_LEVEL	75
+#define VERSION_PATCH_LEVEL	76
 
 //******************
 // Protocols

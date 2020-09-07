e2972a582375b0cb677d9fd987348be2231af8e2
Pascal Langer <planger@mathworks.com>
2020-09-07 20:31:47 +0200

    AFHDS2A: enable 16 channels on SBUS

--- Multiprotocol/Multiprotocol.h
+++ Multiprotocol/Multiprotocol.h
@@ -19,7 +19,7 @@
 #define VERSION_MAJOR		1
 #define VERSION_MINOR		3
 #define VERSION_REVISION	1
-#define VERSION_PATCH_LEVEL	61
+#define VERSION_PATCH_LEVEL	62
 
 //******************
 // Protocols

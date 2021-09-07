ae748e25f88ee247f561a8f579e1fa5acaf12cd4
Pascal Langer <pascal_langer@yahoo.fr>
2021-09-07 23:17:21 +0200

    Update XN297Dump_nrf24l01.ino

--- Multiprotocol/XN297Dump_nrf24l01.ino
+++ Multiprotocol/XN297Dump_nrf24l01.ino
@@ -258,28 +258,6 @@ static uint16_t XN297Dump_callback()
 							debug(" %02X",packet[i]);
 						}
 						debugln("");
-				/*******/
-						#ifdef XERALL
-						packet_sent++;
-						//if(packet_sent==0)
-						{
-							delayMicroseconds(3000);
-							debug("TX");
-							memcpy(packet,(uint8_t *)"\x11\x1A\x35\x43\x01\x00\x00\x00\x00\x1A",10);
-							//memcpy(packet,(uint8_t *)"\x10\x00\x35\x43\x01\x00\x00\x00\x00\x1A",10);
-							XN297_Configure(XN297_CRCEN, XN297_SCRAMBLED, XN297_1M);
-							XN297_SetTXAddr((uint8_t *)"\x56\x06\x23\x00\x13", 5);			// Bind address
-							XN297_SetTxRxMode(TXRX_OFF);
-							XN297_SetTxRxMode(TX_EN);
-							XN297_WriteEnhancedPayload(packet, 0, 0);
-							delayMicroseconds(1000);
-							XN297_WriteEnhancedPayload(packet, 10, 0);
-							delayMicroseconds(1000);
-							NRF24L01_WriteReg(NRF24L01_03_SETUP_AW, 0x01);			// 3 bytes RX/TX address
-							NRF24L01_WriteRegisterMulti(NRF24L01_0A_RX_ADDR_P0, (uint8_t*)"\x55\x0F\x71", 3);	// set up RX address to xn297 preamble
-						}
-						#endif
-				/*******/
 					}
 					else
 					{

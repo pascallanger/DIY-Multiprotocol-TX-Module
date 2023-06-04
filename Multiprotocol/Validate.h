410ce5cc4c7bba60a058048846b4aa1f74412a12
pascallanger <pascal_langer@yahoo.fr>
2023-06-04 12:07:27 +0200

    Fixed SX1276 protocol

--- Multiprotocol/Validate.h
+++ Multiprotocol/Validate.h
@@ -359,7 +359,7 @@
 #if not defined(STM32_BOARD)
 	#undef SX1276_INSTALLED
 #endif
-#ifndef SX1276_INSTALLED || defined MULTI_EU
+#if not defined(SX1276_INSTALLED) || defined MULTI_EU
 	#undef FRSKYR9_SX1276_INO
 #endif
 

3e75c27e950a17c5a352f4f98b3805335bf24df7
pascallanger <pascal_langer@yahoo.fr>
2026-08-02 23:01:33 +0200

    Update Validate.h
    Save Flash space

--- Multiprotocol/Validate.h
+++ Multiprotocol/Validate.h
@@ -396,6 +396,9 @@
 	#undef	KAMTOM_NRF24L01_INO
 	#undef	WL91X_CCNRF_INO
 	#undef	WPL_NRF24L01_INO
+#endif
+
+#if defined(MULTI_AIR) || defined(MCU_STM32F103C8)
 	// Save flash space...
 	#undef	BUMBLEB_CCNRF_INO
 	#undef	CABELL_NRF24L01_INO
@@ -403,7 +406,7 @@
 	#undef	NCC1701_NRF24L01_INO
 	#undef	Q303_CCNRF_INO
 	#undef	Q90C_CCNRF_INO
-	#undef	REDPINE_CC2500_INO#endif
+	#undef	REDPINE_CC2500_INO
 #endif
 
 #ifdef MULTI_SURFACE
